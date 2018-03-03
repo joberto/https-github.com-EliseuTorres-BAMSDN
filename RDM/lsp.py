edd5f15bca12b827fa5ab62e0ba1ee47aff4d11f
Eliseu <eliseutorres.gx@gmail.com>
2018-03-03 16:53:34 -0300

    Update lsp.py

--- RDM/lsp.py
+++ RDM/lsp.py
@@ -2,12 +2,12 @@
 #
 # This file is part of BAMSDN.
 #
-# MininetWeb is free software: you can redistribute it and/or modify
+# BANSDN is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# MininetWeb is distributed in the hope that it will be useful,
+# BANSDN is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
